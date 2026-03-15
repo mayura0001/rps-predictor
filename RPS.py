4373f512851902682105566f0de236df601bbd68
Mayura Jayasinghe <88314486+mayura0001@users.noreply.github.com>
2026-03-15 23:47:04 +0530

    add ignore files and DockerFile

--- RPS.py
+++ RPS.py
@@ -1,5 +1,4 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
-
 '''
 def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
@@ -11,4 +10,6 @@ def player(prev_play, opponent_history=[]):
     return guess
 '''
 
-print('Test Done')
+#This is Mayura Jayasinghe's Implementation for better ML based RPS player
+
+import tensorflow as tf
